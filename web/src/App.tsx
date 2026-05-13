c29af1f42e2f275b53a40c8aed909d9eecd73d9f
Paul Sherwood <paul.sherwood@gmail.com>
2026-05-13 18:21:52 +0100

    ux: scroll to top on navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,16 +3,20 @@ import { Layout } from './components/Layout'
 import { Book } from './pages/Book'
 import { Events } from './pages/Events'
 import { Home } from './pages/Home'
+import { ScrollToTop } from './components/ScrollToTop'
 import './App.css'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="events" element={<Events />} />
-        <Route path="book" element={<Book />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="events" element={<Events />} />
+          <Route path="book" element={<Book />} />
+        </Route>
+      </Routes>
+    </>
   )
 }

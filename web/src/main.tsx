9c9f133950cda953450282559216850da98996aa
Paul Sherwood <paul.sherwood@gmail.com>
2026-05-13 12:21:06 +0100

    fix: add basename to router for github pages

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/merriott-social-venue/">
       <App />
     </BrowserRouter>
   </StrictMode>,
